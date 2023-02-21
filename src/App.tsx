544c2ed7bf458ded2fd00adf5e5f7bfabddc5546
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-02-21 11:01:00 +0100

    Fixed age error

--- src/App.tsx
+++ src/App.tsx
@@ -62,204 +62,212 @@ function App(dto: MedicalFormData) {
 
 	return (
 		<div className='wrapper'>
-		<Container>
-			<h1>Medical History Form</h1>
-			<Form onSubmit={handleSubmit} >
-				<Row>
-					<Col>
-						<Form.Group className="mb-3" controlId="input_firstName">
-							<Form.Label>First Name</Form.Label>
-							<Form.Control
-								required
-								type="text"
-								value={dto.firstName}
-								onChange={e => { updateFields({ firstName: e.target.value }) }}
-							/>
-							<Form.Text className="text-muted">
-								First Name
-							</Form.Text>
-						</Form.Group>
-						<Form.Group className="mb-3" controlId="input_age">
-							<Form.Label>What is your age?</Form.Label>
-							<Form.Control
-								required
-								min={18}
-								max={200}
-								className='select'
-								placeholder="ex: 23"
-								type="number"
-								value={dto.age}
-								onChange={e => { updateFields({ age: e.target.value }) }}
-							/>
-						</Form.Group>
-						<Form.Group className="mb-3" controlId="input_contactNumber">
-							<Form.Label>Contact Number</Form.Label>
-							<PhoneInput
-								required
-								className='select'
-								defaultCountry="ES"
-								placeholder="Enter phone number"
-								type="phone"
-								value={dto.contactNumber}
-								onChange={(value) => updateFields({ contactNumber: value })}
-							/>
+			<Container>
+				<h1>Medical History Form</h1>
+				<Form onSubmit={handleSubmit} >
+					<Row>
+						<Col>
+							<Form.Group className="mb-3" controlId="input_firstName">
+								<Form.Label>First Name</Form.Label>
+								<Form.Control
+									required
+									type="text"
+									value={dto.firstName}
+									onChange={e => { updateFields({ firstName: e.target.value }) }}
+								/>
+								<Form.Text className="text-muted">
+									First Name
+								</Form.Text>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="input_age">
+								<Form.Label>What is your age?</Form.Label>
+								<Form.Control
+									required
+									min={18}
+									max={200}
+									className='select'
+									placeholder="ex: 23"
+									type="number"
+									value={dto.age}
+									onChange={e => { updateFields({ age: Number(e.target.value) }) }}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="input_contactNumber">
+								<Form.Label>Contact Number</Form.Label>
+								<PhoneInput
+									required
+									className='select'
+									defaultCountry="ES"
+									placeholder="Enter phone number"
+									type="phone"
+									value={dto.contactNumber}
+									onChange={(value) => updateFields({ contactNumber: value })}
+								/>
 
-							<Form.Text className="text-muted">
-								(000) 000-0000
-							</Form.Text>
-						</Form.Group>
-					</Col>
-					<Col>
-						<Form.Group className="mb-3" controlId="input_lastName">
-							<Form.Label>Last Name</Form.Label>
-							<Form.Control
-								required
-								type="text"
-								value={dto.lastName}
-								onChange={e => { updateFields({ lastName: e.target.value }) }}
-							/>
-							<Form.Text className="text-muted">
-								Last Name
-							</Form.Text>
-						</Form.Group>
-						<Form.Group controlId="gender_select" className="mb-3" >
-							<Form.Label>What is your gender?</Form.Label>
-							<Form.Select className='select' required onChange={(e) => { updateFields({ gender: Gender[e.target.value as keyof typeof Gender] }) }}>
-								<option>Please Select</option>
-								{Object.keys(Gender)
-									.filter((k: string) => !isNaN(Number(k)))
-									.map((k: string) => Number(k))
-									.map((i: number) => <option key={Gender[i]} value={dto.gender}>{Gender[i]}</option>)
-								}
-							</Form.Select>
-						</Form.Group>
-						<Form.Group className="mb-3" controlId="input_email">
-							<Form.Label>Email address</Form.Label>
-							<Form.Control
-								required
-								type="email"
-								value={dto.email}
-								placeholder="Enter email"
-								onChange={e => { updateFields({ email: e.target.value }) }}
-							/>
-							<Form.Text className="text-muted">
-								example@example.com
-							</Form.Text>
-						</Form.Group>
-					</Col>
-				</Row>
-				<Form.Group controlId="conditions_multiple_select" className="mb-3">
-					<Form.Label>Check the conditions that apply to you or any member of your immediate relatives:</Form.Label>
-					<Form.Control as="select" multiple value={data.conditions} onChange={e => updateFields({ conditions: [].slice.call(e.target.selectedOptions).map(item => item.value) })}>
-						{Object.keys(Conditions)
-							.filter((k: string) => !isNaN(Number(k)))
-							.map((k: string) => Number(k))
-							.map((i: number) => <option key={Conditions[i]} value={Conditions[i]} >{Conditions[i]}</option>)
-						}
-					</Form.Control>
-					<Form.Text className="text-muted">
-						use Ctrl or Command to choose multiple options
-					</Form.Text>
-				</Form.Group>
-				<Form.Group controlId="symptoms_multiple_select" className="mb-3">
-					<Form.Label>Check the symptoms that you' re currently experiencing:</Form.Label>
-					<Form.Control as="select" multiple value={data.symptoms} onChange={e => updateFields({ symptoms: [].slice.call(e.target.selectedOptions).map(item => item.value) })}>
-						{Object.keys(Symptoms)
-							.filter((k: string) => !isNaN(Number(k)))
-							.map((k: string) => Number(k))
-							.map((i: number) => <option key={Symptoms[i]} value={Symptoms[i]} >{Symptoms[i]}</option>)
-						}
-					</Form.Control>
-					<Form.Text className="text-muted">
-						use Ctrl or Command to choose multiple options
-					</Form.Text>
-				</Form.Group>
+								<Form.Text className="text-muted">
+									(000) 000-0000
+								</Form.Text>
+							</Form.Group>
+						</Col>
+						<Col>
+							<Form.Group className="mb-3" controlId="input_lastName">
+								<Form.Label>Last Name</Form.Label>
+								<Form.Control
+									required
+									type="text"
+									value={dto.lastName}
+									onChange={e => { updateFields({ lastName: e.target.value }) }}
+								/>
+								<Form.Text className="text-muted">
+									Last Name
+								</Form.Text>
+							</Form.Group>
+							<Form.Group controlId="gender_select" className="mb-3" >
+								<Form.Label>What is your gender?</Form.Label>
+								<Form.Select className='select' required onChange={(e) => { updateFields({ gender: Gender[e.target.value as keyof typeof Gender] }) }}>
+									<option>Please Select</option>
+									{Object.keys(Gender)
+										.filter((k: string) => !isNaN(Number(k)))
+										.map((k: string) => Number(k))
+										.map((i: number) => <option key={Gender[i]} value={dto.gender}>{Gender[i]}</option>)
+									}
+								</Form.Select>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="input_email">
+								<Form.Label>Email address</Form.Label>
+								<Form.Control
+									required
+									type="email"
+									value={dto.email}
+									placeholder="Enter email"
+									onChange={e => { updateFields({ email: e.target.value }) }}
+								/>
+								<Form.Text className="text-muted">
+									example@example.com
+								</Form.Text>
+							</Form.Group>
+						</Col>
+					</Row>
+					<Form.Group controlId="conditions_multiple_select" className="mb-3">
+						<Form.Label>Check the conditions that apply to you or any member of your immediate relatives:</Form.Label>
+						<Form.Control
+							as="select"
+							multiple
+							value={data.conditions}
+							onChange={e => updateFields({ conditions: [].slice.call(e.target.selectedOptions).map(item => item.value) })}>
+							{Object.keys(Conditions)
+								.filter((k: string) => !isNaN(Number(k)))
+								.map((k: string) => Number(k))
+								.map((i: number) => <option key={Conditions[i]} value={Conditions[i]} >{Conditions[i]}</option>)
+							}
+						</Form.Control>
+						<Form.Text className="text-muted">
+							use Ctrl or Command to choose multiple options
+						</Form.Text>
+					</Form.Group>
+					<Form.Group controlId="symptoms_multiple_select" className="mb-3">
+						<Form.Label>Check the symptoms that you' re currently experiencing:</Form.Label>
+						<Form.Control
+							as="select"
+							multiple
+							value={data.symptoms}
+							onChange={e => updateFields({ symptoms: [].slice.call(e.target.selectedOptions).map(item => item.value) })}>
+							{Object.keys(Symptoms)
+								.filter((k: string) => !isNaN(Number(k)))
+								.map((k: string) => Number(k))
+								.map((i: number) => <option key={Symptoms[i]} value={Symptoms[i]} >{Symptoms[i]}</option>)
+							}
+						</Form.Control>
+						<Form.Text className="text-muted">
+							use Ctrl or Command to choose multiple options
+						</Form.Text>
+					</Form.Group>
 
-				<Form.Group controlId="taking_medicines_radio_buttons" className="mb-3">
-					<Form.Label className="radio-buttons-header">Are you currently taking any medication?
-					</Form.Label>
-					<Form.Check
-						inline
-						label="Yes"
-						value="Yes"
-						name="medication"
-						type={'radio'}
-						onChange={(e) => { updateFields({ medication: true }) }}
-					/>
-					<Form.Check
-						defaultChecked
-						inline
-						label="No"
-						value="No"
-						name="medication"
-						type={'radio'}
-						onChange={(e) => updateFields({ medication: false })}
-					/>
-				</Form.Group>
-				<Form.Group controlId="allergies_radio_buttons" className="mb-3">
-					<Form.Label className="radio-buttons-header">Do you have any medication allergies?
-					</Form.Label>
-					<Form.Check
-						inline
-						label="Yes"
-						value="Yes"
-						name="allergies"
-						type={'radio'}
-						onChange={(e) => { updateFields({ allergies: true }) }}
-					/>
-					<Form.Check
-						defaultChecked
-						inline
-						label="No"
-						value="No"
-						name="allergies"
-						type={'radio'}
-						onChange={(e) => updateFields({ allergies: false })}
-					/>
-				</Form.Group>
-				<Form.Group controlId="smoking_radio_buttons" className="mb-3">
-					<Form.Label className="radio-buttons-header">Do you smoke?
-					</Form.Label>
-					<Form.Check
-						inline
-						label="Yes"
-						value="Yes"
-						name="smoking"
-						type={'radio'}
-						onChange={(e) => { updateFields({ smoking: true }) }}
-					/>
-					<Form.Check
-						defaultChecked
-						inline
-						label="No"
-						value="No"
-						name="smoking"
-						type={'radio'}
-						onChange={(e) => updateFields({ smoking: false })}
-					/>
-				</Form.Group>
-				<Form.Group controlId="alcohol_consum_select" className="mb-3" >
-					<Form.Label>How often do you consume alcohol?</Form.Label>
-					<Form.Select className='select' onChange={(e) => { updateFields({ alcohol: AlcoholConsuming[e.target.value as keyof typeof AlcoholConsuming] }) }}>
-						<option>Please Select</option>
-						{Object.keys(AlcoholConsuming)
-							.filter((k: string) => !isNaN(Number(k)))
-							.map((k: string) => Number(k))
-							.map((i: number) => <option key={AlcoholConsuming[i]} value={dto.alcohol}>{AlcoholConsuming[i]}</option>)
-						}
-					</Form.Select>
-				</Form.Group>
+					<Form.Group controlId="taking_medicines_radio_buttons" className="mb-3">
+						<Form.Label className="radio-buttons-header">Are you currently taking any medication?
+						</Form.Label>
+						<Form.Check
+							inline
+							label="Yes"
+							value="Yes"
+							name="medication"
+							type={'radio'}
+							onChange={(e) => { updateFields({ medication: true }) }}
+						/>
+						<Form.Check
+							defaultChecked
+							inline
+							label="No"
+							value="No"
+							name="medication"
+							type={'radio'}
+							onChange={(e) => updateFields({ medication: false })}
+						/>
+					</Form.Group>
+					<Form.Group controlId="allergies_radio_buttons" className="mb-3">
+						<Form.Label className="radio-buttons-header">Do you have any medication allergies?
+						</Form.Label>
+						<Form.Check
+							inline
+							label="Yes"
+							value="Yes"
+							name="allergies"
+							type={'radio'}
+							onChange={(e) => { updateFields({ allergies: true }) }}
+						/>
+						<Form.Check
+							defaultChecked
+							inline
+							label="No"
+							value="No"
+							name="allergies"
+							type={'radio'}
+							onChange={(e) => updateFields({ allergies: false })}
+						/>
+					</Form.Group>
+					<Form.Group controlId="smoking_radio_buttons" className="mb-3">
+						<Form.Label className="radio-buttons-header">Do you smoke?
+						</Form.Label>
+						<Form.Check
+							inline
+							label="Yes"
+							value="Yes"
+							name="smoking"
+							type={'radio'}
+							onChange={(e) => { updateFields({ smoking: true }) }}
+						/>
+						<Form.Check
+							defaultChecked
+							inline
+							label="No"
+							value="No"
+							name="smoking"
+							type={'radio'}
+							onChange={(e) => updateFields({ smoking: false })}
+						/>
+					</Form.Group>
+					<Form.Group controlId="alcohol_consum_select" className="mb-3" >
+						<Form.Label>How often do you consume alcohol?</Form.Label>
+						<Form.Select className='select' onChange={(e) => { updateFields({ alcohol: AlcoholConsuming[e.target.value as keyof typeof AlcoholConsuming] }) }}>
+							<option>Please Select</option>
+							{Object.keys(AlcoholConsuming)
+								.filter((k: string) => !isNaN(Number(k)))
+								.map((k: string) => Number(k))
+								.map((i: number) => <option key={AlcoholConsuming[i]} value={dto.alcohol}>{AlcoholConsuming[i]}</option>)
+							}
+						</Form.Select>
+					</Form.Group>
 
-				<Form.Group controlId="submit_button_terms" className="mt-5 text-center" >
-					<Button className="mb-3 submit-button" type='submit' size="lg" variant="primary">Submit</Button>
-					<Form.Text className="text-muted">
-						By pressing Submit you agree with our
-						<a href="#">{" "}terms and conditions</a>
-					</Form.Text>
-				</Form.Group>
+					<Form.Group controlId="submit_button_terms" className="mt-5 text-center" >
+						<Button className="mb-3 submit-button" type='submit' size="lg" variant="primary">Submit</Button>
+						<Form.Text className="text-muted">
+							By pressing Submit you agree with our
+							<a href="#">{" "}terms and conditions</a>
+						</Form.Text>
+					</Form.Group>
 
-			</Form>
-		</Container>
+				</Form>
+			</Container>
 		</div>
 	)
 }
